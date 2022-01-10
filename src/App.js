a0aafbeb2c86f3008cd63fa0a8cad645328d4905
IcarusDrowns <amritanshu310@gmail.com>
2022-01-10 11:09:38 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import GoogleLogin from "react-google-login";
 
 function App() {
+  const responseFacebook = (response) => {
+    document.write(
+      `Hey ${response.name} you are logged in with following user id ${response.userID}`
+    );
+  };
+
+  const responseGoogle = (response) => {
+    document.write(
+      `Hey ${response.profileObj.name} you are logged in with ${response.profileObj.email}`
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <FacebookLogin
+          appId="472363297589387"
+          autoLoad
+          callback={responseFacebook}
+          render={(renderProps) => (
+            <button
+              className="btn btn-primary btn-lg btn-block"
+              onClick={renderProps.onClick}
+            >
+              Login with FACEBOOK
+            </button>
+          )}
+        />
+
+        <GoogleLogin
+          clientId="325116987819-oihfd3kr2h9grb4jvo42l8r36i5q8g49.apps.googleusercontent.com"
+          render={(renderProps) => (
+            <button
+              className="btn btn-danger btn-lg btn-block"
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            >
+              Login with GOOGLE
+            </button>
+          )}
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/component/Details.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Details({ props }) {
+  return (
+    <div>
+      <h3>{props.text}</h3>
+    </div>
+  );
+}
